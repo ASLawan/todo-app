c06d131ed0c4046dad444a139d0f1530df8a3403
ASLawan <nawalnitsua@gmail.com>
2024-06-22 23:44:24 +0100

    Implement interactive behaviour and validation with js

--- FORM_VALIDATION/js/main.js
+++ FORM_VALIDATION/js/main.js
@@ -8,15 +8,39 @@
 // keep it DRY
 
 let getId = (id) => document.getElementById(id);
-let getclass = (className) => document.getElementsByClassName(className);
+let getClass = (className) => document.getElementsByClassName(className);
 
 let username = getId("username"),
     email = getId("email"),
     password = getId("password"),
+    form = getId("form"),
     errorMsg = getClass("error"),
     successIcon = getClass("success_icon"),
-    failIcon = getclass("fail_icon");
+    failIcon = getClass("fail_icon");
 
 
     // errorMsg = ['error', 'error', 'error'];
     // errorMsg[2].innerHTML = 'Password error';
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        engine(username, 0, "Username cannot be blank!");
+        engine(email, 1, "Email cannot be blank!");
+        engine(password, 2, "Password cannot be blank!");
+        
+    });
+
+    // validation engine
+
+    let engine = (id, serial, msg) => {
+        if (id.value.trim() === ""){
+            errorMsg[serial].innerHTML = msg;
+            failIcon[serial].style.opacity = "1";
+            successIcon[serial].style.opacity = "0"
+        }  else {
+            errorMsg[serial].innerHTML = "";
+            successIcon[serial].style.opacity = "1"
+            failIcon[serial].style.opacity = "0"
+        }
+    }
